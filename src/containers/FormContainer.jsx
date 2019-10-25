00b95ae9cca9539a6327b04f207988e37ed6ff65
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2019-10-25 04:43:39 -0500

    se implementa correctamente los eventos onSubmit y onSubmitSuccess

--- src/containers/FormContainer.jsx
+++ src/containers/FormContainer.jsx
@@ -5,50 +5,25 @@ class FormContainer extends React.Component {
     constructor() {
         super();
         this.state = {
-          totalAmount: "",
-          discount: "",
-          applyDiscount: false,
-          showList: false,
+          valuesForm:''
         };
       }
 
-      hideListUser = () => {
-          this.setState({
-            showList: false,
-          })
+      handleSubmitForm = values => {
+        this.setState({valuesForm: values})  
+        return values;
       }
 
-      showListUser = () => {
-        this.setState({
-            showList: true,
-          })
-      }
-
-      handleChange = (event) => {
-          const newAmount = parseInt(event.target.value) * this.props.insuredUsers.length;
-          this.setState({totalAmount: newAmount}, () => console.log(this.state.totalAmount))
-      }
-
-      handleChangeDiscountCode = (event) => {
-          const amountDiscount = event.target.value.substring(0,2);
-          this.setState({discount: amountDiscount}, () => console.log(amountDiscount))
-      }
-
-      handleApplyDiscount = () => {
-        const newAmountDiscount = this.state.totalAmount - (this.state.totalAmount * parseInt(this.state.discount) / 100);
-        this.setState({
-            totalAmount: newAmountDiscount,
-            applyDiscount: true,
-        })
-      }
-
-      handleSubmitThirdStep = () => {
-          this.props.updateStep()
-      }
+      handleSuccessForm = res => {
+        console.log('El cambio de estado fue exitoso');
+        console.log(res);
+        
+    }
 
     render() {
         return <Form 
-        handleSubmit = {this.handleSubmitThirdStep}
+        onSubmit = {this.handleSubmitForm}
+        onSubmitSuccess = {this.handleSuccessForm}
         />
     }
 }
